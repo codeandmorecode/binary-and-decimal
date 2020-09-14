13562fbb4b97663959e0cb9f0ec5689f4a924526
codeandmorecode <68569740+codeandmorecode@users.noreply.github.com>
2020-09-14 16:06:58 -0500

    Update binary_to_decimal.rb

--- lib/binary_to_decimal.rb
+++ lib/binary_to_decimal.rb
@@ -1,9 +1,20 @@
-# A method named `binary_to_decimal` that receives as input an array of size 8.
-# The array is randomly filled with 0’s and 1’s.
-# The most significant bit is at index 0.
-# The least significant bit is at index 7.
-# Calculate  and return the decimal value for this binary number using
-# the algorithm you devised in class.
-def binary_to_decimal(binary_array)
-  raise NotImplementedError
+# Defines method binary_to_decimal
+def binary_to_decimal (str)
+  sum = 0
+
+  i = -8
+  until i == 0 do
+    if str[i] == 1
+      sum += (2**(i.abs - 1))
+    end
+    i += 1
+  end
+
+  return sum
 end
+
+# Defines random string of 0 and 1's
+str = (1..8).map { [0, 1].sample }
+
+# Calls the binary_and_deciaml method within string concatenation and displays result to user
+puts "#{str.join} base 2 in decimal is #{binary_to_decimal(str)}."
